e8820ddf17fea1e4eb8f164f1f713517117c8603
Jon Sykes <jongit@sykes.me>
2011-10-11 10:38:19 -0400

    Fixed namespace, updated base js file

---
+++ jquery.jTaB.js
@@ -0,0 +1,43 @@
+/*!
+ * jQuery jTaB: Dynamic Tab Index Management - v1.0pre - ??/??/2010
+ * 
+ * Copyright (c) 2010 Ken Wheeler / Jon Sykes
+ * Dual licensed under the MIT and GPL licenses.
+ * Free as in Bacon
+ */
+
+// Script: jQuery jTaB: Dynamic Tab Index Management
+//
+// *Version: 1.0pre, Last updated: ??/??/2010*
+// 
+// GitHub       - http://github.com/kdubbicles/jTab/
+// Source       - http://github.com/kdubbicles/jTab/raw/master/jquery.ba-XXX.js
+// (Minified)   - http://github.com/kdubbicles/jTab/raw/master/jquery.ba-XXX.min.js (X.Xkb)
+// 
+// About: Examples
+// 
+// These working examples, complete with fully commented code, illustrate a few
+// ways in which this plugin can be used.
+// 
+// jTaB - http://github.com/kdubbicles/jTab/examples/XXX/
+// 
+// About: Support and Testing
+// 
+// Information about what version or versions of jQuery this plugin has been
+// tested with, what browsers it has been tested in, and where the unit tests
+// reside (so you can test it yourself).
+// 
+// jQuery Versions - 1.4.2, 1.3.2
+// Browsers Tested - Internet Explorer 6-8, Firefox 3-3.6, Safari 3-4, Chrome 3-5, Opera 9.6-10.5.
+// Unit Tests      - http://github.com/kdubbicles/jTab/unit
+// 
+// About: Release History
+// 
+// 1.0pre - (??/??/2010) Pre-Initial release
+
+(function($){
+  '$:nomunge'; // Used by YUI compressor.
+  
+  // code
+  
+})(jQuery);
